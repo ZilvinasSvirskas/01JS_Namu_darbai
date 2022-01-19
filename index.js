de9bdb2e56d5273ff92003647434e1a4c980b87c
Zilvinas Svirskas <97021844+ZilvinasSvirskas@users.noreply.github.com>
2022-01-19 09:20:23 +0200

    Index.js

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const mokinys4 = "Vida";
 const mokiniuvardai = (mokinys1 + "," +mokinys2 +"," + mokinys3 + "," + mokinys4 );
 console.log("Mokiniu vardai:",mokiniuvardai);
 
-console.log("Mini uzduotys")
 
 
 // Pazymiai: 10,2,8,6,4.
@@ -44,3 +43,47 @@ console.log("Pazimiu kiekis:", pazymiuKiekis.length)
 const pazymiuVidurkis = pazymiuSuma/5
 console.log("Pazymiu vidurkis:",pazymiuVidurkis)
 
+console.log("Mini uzduotys")
+//Kintamųjų inicijavimas
+//Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const skaicius1 = 10
+const skaicius2 = 20
+const skaicius3 = 30
+
+console.log(skaicius1);
+console.log(skaicius2);
+console.log(skaicius3);
+
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const pirmasZodis = "Mano";
+const antrasZodis = "vardas";
+const treciasZodis = "Zilvinas";
+console.log(pirmasZodis);
+console.log(antrasZodis);
+console.log(treciasZodis);
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const sarasas = [10,9,8,7,6]
+const pazymiai = [5,4,3,2,1]
+const iverinimai = [15,14,13,12,11]
+
+console.log("Sarasas",sarasas);
+console.log("Pazymiai",pazymiai);
+console.log("Ivertinimai",iverinimai);
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+
+const prekiuSarasas = ['obuoliai', 'pienas', 'pomidorai', 'agurkai', 'svogunai']
+console.log("Prekiu sarasas:",prekiuSarasas);
+const miestuSarasas = ['Vilnius','Kaunas','Klaipeda','Siauliai','Panevezys']
+console.log("Miestu sarasas:",miestuSarasas);
+const dalyviuSarasas = ['Zilvinas','Lina','Marius','Vida','Austeja']
+console.log("Dalyviu saras",dalyviuSarasas)
