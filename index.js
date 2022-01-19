034aae6edb0bdb482f1b112f85435907bafc0abb
Zilvinas Svirskas <97021844+ZilvinasSvirskas@users.noreply.github.com>
2022-01-19 08:20:48 +0200

    Index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+// Klaseje mokosi 4 asmenys.
+//Mokiniu vardai: Gediminas,Paulius,Ruta,Vida.
+
+const asmuo1 = "mokinys1";
+const asmuo2 = "mokinys2";
+const asmuo3 = "mokinys3";
+const asmuo4 = "mokinys4";
+
+const asmenys = [asmuo1,asmuo2,asmuo3,asmuo4];
+console.log(asmenys);
+
+
+const mokinys1 = "Gediminas";
+const mokinys2 = "Paulius";
+const mokinys3 = "Ruta";
+const mokinys4 = "Vida";
+
+const mokiniuvardai = (mokinys1 + "," +mokinys2 +"," + mokinys3 + "," + mokinys4 );
+console.log("Mokiniu vardai:",mokiniuvardai);
+
+
+// Pazymiai: 10,2,8,6,4.
+// Pazymiu suma: 30.
+// Pazymiu kiekis: 5. 
+// Pazymiu vidurkis: 6.
+
+const pazimys1 = 10;
+const pazimys2 = 2;
+const pazimys3 = 8;
+const pazimys4 = 6;
+const pazimys5 = 4;
+
+const Pazymiai = (pazimys1 + "," + pazimys2 + "," + pazimys3 + "," + pazimys4 + "," + pazimys5);
+console.log("Pazymiai:",Pazymiai);
+
+const pazymiuSuma = pazimys1 + pazimys2 + pazimys3 + pazimys4 + pazimys5
+console.log("Pazymiu suma:",pazymiuSuma);
+
+
+console.log("Pazimiu kiekis:", Pazymiai.length)
+
+const pazymiuVidurkis = pazymiuSuma/5
+console.log("Pazymiu vidurkis:",pazymiuVidurkis)
+
